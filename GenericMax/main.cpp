f0384e1a90c1ef4bb72b0fb729270b5566977026
moshegowers <moshegowers@gmail.com>
2017-11-15 12:11:54 +0200

    add comparison criteria

--- GenericMax/main.cpp
+++ GenericMax/main.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
 
-template <typename T>
-T Max(T first, T sconde, T three)
+template <typename T, typename COMP>
+T Max(T first, T sconde, T three, COMP comp)
 {
-	return first > sconde ? (first > three ? first : three) : (sconde > three ? sconde : three);
+	return comp(first, sconde, three);
 }
+
+class StringComperer
+{
+public:
+	StringComperer(bool isCaseSensitive):m_isCaseSensitive(isCaseSensitive)
+	{}
+	char * operator()(char *first, char *sconde, char *three)
+	{
+		char *res = m_isCaseSensitive ?
+			strcmp(first, sconde) > 0 ? (strcmp(first, three) > 0 ? first : three) : (strcmp(sconde, three) > 0 ? sconde : three)
+			: stricmp(first, sconde) > 0 ? (strcmp(first, three) > 0 ? first : three) : (strcmp(sconde, three) > 0 ? sconde : three);
+
+		return res;
+	}
+private:
+	bool m_isCaseSensitive;
+};
 
 void main(void)
 {
-	std::cout << "Int: " << Max<int>(1, 3, 2) << std::endl;
-	std::cout << "Double: " << Max<double>(1.1, 2.2, 3.3) << std::endl;
+	char *s1 = "abc", *s2 = "ABC", *s3 = "Acd";
+	std::cout << "Sensitive: " << Max(s1, s2, s3, StringComperer(true)) << std::endl;
+	std::cout << "Insensitive: " << Max(s1, s2, s3, StringComperer(false)) << std::endl;
 }
